7d1287b0594266eb888d5c5cc9efac5cf4e5b56e
Viktor Hagag <74266729+viktorhagag@users.noreply.github.com>
2025-08-05 16:24:53 +0300

    Add files via upload

---
+++ MaakavLimud/ContentView.swift
@@ -0,0 +1,14 @@
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("מעקב לימוד")
+            .font(.largeTitle)
+            .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MaakavLimud/MaakavLimud.swift
@@ -0,0 +1,11 @@
+
+import SwiftUI
+
+@main
+struct MaakavLimud: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
